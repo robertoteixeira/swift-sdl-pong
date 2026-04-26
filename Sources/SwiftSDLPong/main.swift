23721eb08251b1fc0be67edb5580b15f1483486b
Roberto Teixeira <roberto.teixeirafs@gmail.com>
2026-04-26 17:05:57 -0300

    Render paddles and ball

--- Sources/SwiftSDLPong/main.swift
+++ Sources/SwiftSDLPong/main.swift
@@ -43,6 +43,31 @@ defer {
     SDL_DestroyRenderer(renderer)
 }
 
+let paddleWidth: Float = 15
+let paddleHeight: Float = 100
+let ballSize: Float = 15
+
+var leftPaddle = SDL_FRect(
+    x: 40,
+    y: Float(screenHeight) / 2 - paddleHeight / 2,
+    w: paddleWidth,
+    h: paddleHeight
+)
+
+var rightPaddle = SDL_FRect(
+    x: Float(screenWidth) - 40 - paddleWidth,
+    y: Float(screenHeight) / 2 - paddleHeight / 2,
+    w: paddleWidth,
+    h: paddleHeight
+)
+
+var ball = SDL_FRect(
+    x: Float(screenWidth) / 2 - ballSize / 2,
+    y: Float(screenHeight) / 2 - paddleHeight / 2,
+    w: ballSize,
+    h: ballSize
+)
+
 var isRunning = true
 var event = SDL_Event()
 
@@ -55,6 +80,12 @@ while isRunning {
 
     SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255)
     SDL_RenderClear(renderer)
+
+    SDL_SetRenderDrawColor(renderer, 240, 240, 240, 255)
+
+    SDL_RenderFillRect(renderer, &leftPaddle)
+    SDL_RenderFillRect(renderer, &rightPaddle)
+    SDL_RenderFillRect(renderer, &ball)
 
     SDL_RenderPresent(renderer)
 }
